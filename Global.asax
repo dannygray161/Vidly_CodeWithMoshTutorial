33c3743ef1c162f7e4b37002f817696a7e35ae7a
Daniel Gray <dieseldan14@gmail.com>
2021-06-16 13:11:41 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly_CodeWithMoshTutorial.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_CodeWithMoshTutorial.Startup))]
+namespace Vidly_CodeWithMoshTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
